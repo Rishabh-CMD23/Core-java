1d4198c7644ab44c98c7ed4082e25c65f54e2da8
Rishabh-CMD23 <168037972+Rishabh-CMD23@users.noreply.github.com>
2024-08-02 17:18:06 +0530

    Add files via upload

---
+++ Day_2_medium2_thread.java
@@ -0,0 +1,44 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ldc;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Day_2_medium2_thread  {
+    public static volatile int a=1;
+    static class a extends Thread{
+        public void run(){
+            int l=a;
+            while(l<5){
+                if(l!=a){
+                    System.out.println("got value of local value is "+l);
+                    l=a;
+                }
+            }
+        }
+    }
+    static class b extends Thread{
+        public void run(){
+            int l=a;
+            while(a<5){
+                System.out.println("incremented value of a is " + (l+1));
+                a=++l;
+                try {
+                    Thread.sleep(100);
+                } catch (Exception e) {
+                    System.out.println(e);
+                }
+            }
+        }
+    }
+    public static void main(String[] args){
+        new a().start();
+        new b().start();
+        
+        
+    }
+}
